d365f1289612a82116217c2445cf6b29407dc826
LbzSteven <zkan@ucdavis.edu>
2024-06-22 22:27:49 -0700

    0622 before change the stopping criterion of TSEvo and target class of NG NUN

--- evalutation.py
+++
@@ -1,6 +0,0 @@
-from utils.model_util import get_AE_dict
-
-AE_dict = get_AE_dict('FCN_AE', '../models/AE/GunPoint', 1, 150)
-for keys, values in AE_dict.items():
-    print(keys)
-    # print(values)
---
+++ run_wCF.sh
@@ -0,0 +1,7 @@
+#python CF_generate.py  --CF_name wCF --model_name FCN --dataset_choice selected_uni --CUDA cuda:1 --start_per 0.0 --end_per 0.2 &
+#python CF_generate.py  --CF_name wCF --model_name FCN --dataset_choice selected_uni --CUDA cuda:1 --start_per 0.2 --end_per 0.4 &
+#python CF_generate.py  --CF_name wCF --model_name FCN --dataset_choice selected_uni --CUDA cuda:2 --start_per 0.4 --end_per 0.6 &
+#python CF_generate.py  --CF_name wCF --model_name FCN --dataset_choice selected_uni --CUDA cuda:2 --start_per 0.6 --end_per 0.8 &
+#python CF_generate.py  --CF_name wCF --model_name FCN --dataset_choice selected_uni --CUDA cuda:3 --start_per 0.8 --end_per 1.0 &
+
+wait
